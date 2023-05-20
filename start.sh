791f9f22d61a23e67d6d132cee03faee2cfa2e99
rajraswin51 <129712623+rajraswin51@users.noreply.github.com>
2023-05-20 16:58:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/auto-filter-bot-v3.git /auto-filter-bot-v3
+git clone https://github.com/rajraswin51/auto-filter-bot-v3 /auto-filter-bot-v3
 cd /auto-filter-bot-v3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
