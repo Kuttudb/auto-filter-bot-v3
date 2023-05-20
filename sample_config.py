fe37d3050eea1b155c3cba877f4ea52e68bf0898
rajraswin51 <129712623+rajraswin51@users.noreply.github.com>
2023-05-20 16:28:12 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) @ZauteKm
+# Copyright (C) @armovichat
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -14,31 +14,41 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Bot information
-SESSION = 'BetterAutoFilterBot'
+SESSION = 'dorafilmbot'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:BetterAutoFilterBot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'BetterAutoFilterBot'
+API_ID = 15056052
+API_HASH = '87f3fa0ad63a2b9dc87f2fd6def04970'
+BOT_TOKEN = '5787728341:AAG2eBIQZfLw6rQH0eDuy7wytbUHwQsVsmY'
+USERBOT_STRING_SESSION = 'dorapublicationbot'
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'TGBotsProJect']
+ADMINS = [5009480460]
+CHANNELS = [-1001607950761, 'armoviechat']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[BetterAutoFilterBot:BetterAutoFilterBot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'BetterAutoFilterBot'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+DATABASE_URI = "mongodb+srv://Olammovie:LfbR3H731Q8pbtSl@cluster0.tipaq8j.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'olammovies'
+COLLECTION_NAME = 'dora_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**𝖸𝗈...𝖸𝗈... Hey I'm DORA FILMS 💖
+
+𝖨'𝗆 𝖯𝗈𝗐𝖾𝗋𝖿𝗎𝗅 𝖠𝗎𝗍𝗈-𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍 𝖸𝗈𝗎 𝖢𝖺𝗇 𝖴𝗌𝖾 𝖬𝖾 𝖠𝗌 𝖠 𝖠𝗎𝗍𝗈-𝖿𝗂𝗅𝗍𝖾𝗋 𝗂𝗇 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉
+
+𝖨𝗍𝗌 𝖤𝖺𝗌𝗒 𝖳𝗈 𝖴𝗌𝖾 𝖬𝖾; 𝖩𝗎𝗌𝗍 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 𝖠𝗌 𝖠𝖽𝗆𝗂𝗇, 
+
+𝖳𝗁𝖺𝗍𝗌 𝖠𝗅𝗅, 𝗂 𝗐𝗂𝗅𝗅 𝖯𝗋𝗈𝗏𝗂𝖽𝖾 𝖬𝗈𝗏𝗂𝖾𝗌 𝖳𝗁𝖾𝗋𝖾...🤓🤪
+
+⚠️𝖬𝗈𝗋𝖾 𝖧𝖾𝗅𝗉 𝖧𝗂𝗍 /help
+
+😎 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖻𝗒 @armoviechat**
 
 Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
